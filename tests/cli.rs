921c7c1ca03e2b247ff82c54d68d972fb5412c88
Anima <18208134+animafps@users.noreply.github.com>
2022-12-19 14:28:35 +1300

    ci: added test checking

--- tests/cli.rs
+++ tests/cli.rs
@@ -7,9 +7,9 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("teres")?;
 
     cmd.arg("test/file/doesnt/exist").arg("-n");
-    cmd.assert()
-        .failure()
-        .stderr(predicate::str::contains("Video test/file/doesnt/exist does not exist"));
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "Video test/file/doesnt/exist does not exist",
+    ));
 
     Ok(())
 }
